6265985cb10951fc6e5d9ed2f2cb06eb36350135
gabrielrodriguesleite <k.cogabriel@gmail.com>
2023-04-14 08:16:09 -0300

    crawler development - getResponse ok

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
 func main() {
-	print("\nok\n")
+	fmt.Println(getResponse("https://lista.mercadolivre.com.br/veiculos/fusca"))
+}
+
+type Item struct {
+	Photo       string
+	Description string
+	Category    string
+	Price       string
+	Website     string
+}
+
+func getResponse(url string) *http.Response {
+	time.Sleep(time.Duration(rand.Intn(5)))
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.StatusCode != 200 {
+		panic(fmt.Sprintf("status code error: %d %s", res.StatusCode, res.Status))
+	}
+
+	return res
 }
