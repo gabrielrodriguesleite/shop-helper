a3cddce184a1faa5c21ad6b0bdd25237d39b432a
gabrielrodriguesleite <k.cogabriel@gmail.com>
2023-04-14 09:18:10 -0300

    crawlerAPI server on

--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,10 @@ module backend
 
 go 1.18
 
-require github.com/PuerkitoBio/goquery v1.8.1
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/gorilla/mux v1.8.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"fmt"
-
 	"backend/crawler"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	items := crawler.GetMLItems("veiculos", "fusca")
+	// items := crawler.GetMLItems("veiculos", "fusca")
 
-	print("\n\nPrice Crawler:\n\n")
+	// print("\n\nPrice Crawler:\n\n")
 
-	for i, item := range items {
-		fmt.Printf("Item %d\n %+v\n", i+1, item)
-	}
+	// for i, item := range items {
+	// 	fmt.Printf("Item %d\n %+v\n", i+1, item)
+	// }
 
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", Get).Methods(http.MethodGet)
+
+	fmt.Println("listen at http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	json.
+		NewEncoder(w).
+		Encode(
+			crawler.Item{
+				Description: "uma descrição do produto",
+			},
+		)
 }
